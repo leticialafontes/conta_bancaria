5c594a5a113f1fb329401e98d65fe51e98daee03
leticialafontes <leticia.abib95@gmail.com>
2025-06-04 15:16:28 -0300

    Inclusão de conta corrente e conta poupança, classe conta abstrata e iniciação de interface

--- Menu.ts
+++ Menu.ts
@@ -1,17 +1,37 @@
 import read from 'readline-sync'
 import { colors } from './src/util/Colors';
 import { Conta } from './src/model/Conta';
+import { ContaCorrente } from './src/model/ContaCorrente';
+import { ContaPoupanca } from './src/model/ContaPoupanca';
 
 export function mainMenu() {
 
     let option: number;
 
-    const conta: Conta = new Conta(1, 123, 1, "Adriana", 10000); //teste para ver se a classe conta foi feita corretamente
-    conta.visualizar();
-    conta.sacar(10500);
-    conta.visualizar();
-    conta.depositar(5000);
-    conta.visualizar();
+    //teste Conta
+    // const conta: Conta = new Conta(1, 123, 1, "Adriana", 10000); //teste para ver se a classe conta foi feita corretamente
+    // conta.visualizar();
+    // conta.sacar(10500);
+    // conta.visualizar();
+    // conta.depositar(5000);
+    // conta.visualizar();
+
+    //teste ContaCorrente
+
+    const contacorrente: ContaCorrente = new ContaCorrente (2, 123, 1, "Mariana", 15000, 1000);
+    contacorrente.visualizar();
+    contacorrente.sacar(2000);
+    contacorrente.visualizar();
+    contacorrente.depositar(1000);
+    contacorrente.visualizar();
+
+    // teste ContaPoupanca
+    const contapoupanca: ContaPoupanca = new ContaPoupanca(3, 123, 2, "Victor", 1000, 10);
+    contapoupanca.visualizar();
+    contapoupanca.sacar(200);
+    contapoupanca.visualizar();
+    contapoupanca.depositar(1000);
+    contapoupanca.visualizar();
 
     while (true) {
 
